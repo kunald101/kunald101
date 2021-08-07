6325f499f06037083e56717dcb18c4cc01230cc0
kunald101 <kunalpd20@gmail.com>
2021-08-07 16:32:03 -0700

    Deploy app to Heroku

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-
-const app = express();
+// server/index.js
+const path = require('path');
+const express = require('express');
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
